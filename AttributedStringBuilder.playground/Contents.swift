409d9a46207de4afd8dc878db055aed16bca5cce
Andrey Bogushev <andrey.bogushev@betterme.world>
2019-04-11 18:49:40 +0300

    Refactored Api.

--- AttributedStringBuilder.playground/Contents.swift
+++ AttributedStringBuilder.playground/Contents.swift
@@ -2,64 +2,108 @@ import UIKit
 import Foundation
 
 class AttributedStringBuilder {
-    let string: NSString
-    private let fullRange: NSRange
-    private var addAttributes: (NSMutableAttributedString) -> Void = { _ in }
-
-    init(string: String) {
-        self.string = (string as NSString)
-        self.fullRange = NSRange(location: 0, length: self.string.length)
+    enum Attribute {
+        case font(UIFont)
+        case foregroundColor(UIColor)
+        case paragraphStyle(NSParagraphStyle)
+        
+        var keyValue: (NSAttributedString.Key, Any) {
+            switch self {
+            case .font(let font):
+                return (.font, font)
+            case .foregroundColor(let color):
+                return (.foregroundColor, color)
+            case .paragraphStyle(let style):
+                return (.paragraphStyle, style)
+            }
+        }
+    }
+    
+    private let string: NSString
+    private let range: NSRange
+    private var commands: [(NSMutableAttributedString) -> Void] = []
+    private let ancestorString: (() -> NSAttributedString)?
+    
+    convenience init(string: String, attributes: [Attribute] = []) {
+        self.init(string: string, attributes: attributes, ancestorString: nil)
+    }
+    
+    convenience init(string: String, attributes: Attribute...) {
+        self.init(string: string, attributes: attributes)
+    }
+    
+    private init(string: NSString, range: NSRange, commands: [(NSMutableAttributedString) -> Void], ancestorString: (() -> NSAttributedString)?) {
+        self.string = string
+        self.range = range
+        self.commands = commands
+        self.ancestorString = ancestorString
+    }
+    
+    private init(string: String, attributes: [Attribute], ancestorString: (() -> NSAttributedString)?) {
+        self.string = string as NSString
+        self.range = NSRange(location: 0, length: self.string.length)
+        self.ancestorString = ancestorString
+        _ = addAttributes(attributes)
     }
     
     func make() -> NSAttributedString {
-        let attributedString = NSMutableAttributedString(string: string as String)
-        addAttributes(attributedString)
-        return NSAttributedString(attributedString: attributedString)
-    }
-
-    func font(_ font: UIFont) -> AttributedStringBuilder {
-        return appendAttribute(.font, value: font)
-    }
-    
-    func font(_ font: UIFont, substring: String) -> AttributedStringBuilder {
-        return appendAttribute(.font, value: font, toSubstring: substring)
-    }
-    
-    func foregroundColor(_ color: UIColor) -> AttributedStringBuilder {
-        return appendAttribute(.foregroundColor, value: color)
-    }
-    
-    func foregroundColor(_ color: UIColor, substring: String) -> AttributedStringBuilder {
-        return appendAttribute(.foregroundColor, value: color, toSubstring: substring)
-    }
-    
-    func paragraphStyle(_ paragraphStyle: NSParagraphStyle) -> AttributedStringBuilder {
-        return appendAttribute(.paragraphStyle, value: paragraphStyle)
-    }
-    
-    func paragraphStyle(_ paragraphStyle: NSParagraphStyle, substring: String) -> AttributedStringBuilder {
-        return appendAttribute(.paragraphStyle, value: paragraphStyle, toSubstring: substring)
-    }
-    
-    private func appendAttribute(_ attribute: NSAttributedString.Key, value: Any) -> AttributedStringBuilder {
-        addAttributes = { [fullRange, addAttributes] in
-            addAttributes($0)
-            $0.addAttribute(attribute, value: value, range: fullRange)
-        }
-        return self
-    }
-    
-    private func appendAttribute(_ attribute: NSAttributedString.Key, value: Any, toSubstring substring: String) -> AttributedStringBuilder {
-        let range = string.range(of: substring)
+        var result = NSMutableAttributedString(string: string as String)
+        commands.forEach { $0(result) }
         
-        if range.location != NSNotFound {
-            addAttributes = { [addAttributes] in
-                addAttributes($0)
-                $0.addAttribute(attribute, value: value, range: range)
+        if let ancestorString = (ancestorString?()).map(NSMutableAttributedString.init) {
+            ancestorString.append(result)
+            result = ancestorString
+        }
+        
+        return NSAttributedString(attributedString: result)
+    }
+    
+    func addAttributes(_ attributes: Attribute...) -> AttributedStringBuilder {
+        return self.addAttributes(attributes)
+    }
+    
+    func addAttributes(_ attributes: [Attribute]) -> AttributedStringBuilder {
+        return addAttributes(attributes, toRange: range)
+    }
+    
+    func modifySubstring(_ substring: String, attributes: Attribute...) -> AttributedStringBuilder {
+        return self.modifySubstring(substring, attributes: attributes)
+    }
+    
+    func modifySubstring(_ substring: String, attributes: [Attribute]) -> AttributedStringBuilder {
+        let substringRange = string.range(of: substring)
+        
+        if substringRange.location != NSNotFound {
+            return addAttributes(attributes, toRange: substringRange)
+        } else {
+            return self
+        }
+    }
+    
+    func append(string: String, attributes: Attribute...) -> AttributedStringBuilder {
+        return self.append(string: string, attributes: attributes)
+    }
+    
+    func append(string: String, attributes: [Attribute]) -> AttributedStringBuilder {
+        let copy = self.copy()
+        return AttributedStringBuilder(string: string, attributes: attributes, ancestorString: { copy.make() })
+    }
+    
+    private func addAttributes(_ attributes: [Attribute], toRange range: NSRange) -> AttributedStringBuilder {
+        if !attributes.isEmpty {
+            let command = { (attributedString: NSMutableAttributedString) in
+                let attributes: [NSAttributedString.Key: Any] = attributes.reduce(into: [:]) { $0[$1.keyValue.0] = $1.keyValue.1 }
+                attributedString.addAttributes(attributes, range: range)
             }
+            
+            commands.append(command)
         }
         
         return self
+    }
+    
+    private func copy() -> AttributedStringBuilder {
+        return AttributedStringBuilder(string: string, range: range, commands: commands, ancestorString: ancestorString)
     }
 }
 
@@ -68,11 +112,9 @@ let source = "Hello😀, 😄world!"
 let boldPart = "Hello"
 let redPart = "world"
 
-let attributedResult = AttributedStringBuilder(string: source)
-                        .foregroundColor(.yellow)
-                        .font(.boldSystemFont(ofSize: 20), substring: boldPart)
-                        .font(.boldSystemFont(ofSize: 20), substring: redPart)
-                        .foregroundColor(.red, substring: redPart)
+let attributedResult = AttributedStringBuilder(string: source, attributes: .foregroundColor(.yellow))
+                        .modifySubstring(boldPart, attributes: .font(.boldSystemFont(ofSize: 20)))
+                        .modifySubstring(redPart, attributes: .font(.boldSystemFont(ofSize: 20)), .foregroundColor(.red))
                         .make()
 
 let label = UILabel(frame: CGRect(x: 0, y: 0, width: 300, height: 30))
